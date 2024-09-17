7b3e8ee66a41e2ec972b52d2d6b71f459ca2e8e7
rafael <rafael.perez.grisales@gmail.com>
2024-09-17 11:54:03 -0500

    cambios package.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { Section1Component } from './components/section1/section1.component';
+import { Section2Component } from './components/section2/section2.component';
+import { Section3Component } from './components/section3/section3.component';
+import { Section4Component } from './components/section4/section4.component';
+import { Section5Component } from './components/section5/section5.component';
+import { Section6Component } from './components/section6/section6.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    Section1Component,
+    Section2Component,
+    Section3Component,
+    Section4Component,
+    Section5Component,
+    Section6Component
   ],
   imports: [
     BrowserModule,
